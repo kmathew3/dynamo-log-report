80f2efa79be9643b4a3e773b843b70afb582c196
kmathew3 <kmathew3@terpmail.umd.edu>
2026-07-07 16:17:13 -0400

    fix reward output

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir -p /logs/verifier
+
 pytest /tests/test_outputs.py -rA
 
 if [ $? -eq 0 ]; then
-  echo 1 > /verifier/reward.txt
+  echo 1 > /logs/verifier/reward.txt
 else
-  echo 0 > /verifier/reward.txt
+  echo 0 > /logs/verifier/reward.txt
 fi
